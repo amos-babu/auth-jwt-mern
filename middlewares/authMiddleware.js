9338bb79517c0206df2e14f5b0ce2eb039b9085b
Amos Babu <amos.babu@yahoo.com>
2025-05-12 10:12:52 +0300

    modifying the verifyToken middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,18 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
-    const token = req.header('Authorization');
+    const authHeader = req.header('Authorization');
 
-    if(!token){
-        return res.status({ error: "Access Denied" });
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: "Access Denied" });
     }
+
+    const token = authHeader.split(' ')[1];
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         req.userId = decoded.userId;
         next();
     } catch (error) {
-        res.status(500).json({ error: "Invalid Token!" });
+        res.status(500).json({ message: "Invalid Token!", error });
     }
 }
 
